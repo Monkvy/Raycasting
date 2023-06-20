96a5ecd9661126228ef243763e2bab72d800cc46
BuildTools <moluecke@gmx.de>
2023-06-20 13:48:53 +0200

    visim hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+
+fn main() -> visim::Result<()> {
+    let mut window = visim::Window::new("Raytracing", 1280, 720)?;
+
+    while window.is_open() {
+        window.get_events();
+        window.update();
+    }
+
+    Ok(())
 }
